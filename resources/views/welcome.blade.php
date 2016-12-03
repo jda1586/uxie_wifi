a887b62c030aed300803885431418faafafad8f9
Alfonso Rodriguez <darkdreke@gmail.com>
2016-12-02 22:57:52 -0600

    welcome view added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,109 +1,71 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+    <title>Laravel</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-        <link href="{{asset('css/app.css')}}" rel="stylesheet" type="text/css">
-        <link href="{{asset('css/materialize.css')}}" rel="stylesheet" type="text/css">
-        <link href="{{asset('mdi/css/materialdesignicons.min.css')}}" media="all" rel="stylesheet" type="text/css" />
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <link href="{{asset('css/materialize.css')}}" rel="stylesheet" type="text/css">
+    <link href="{{asset('mdi/css/materialdesignicons.min.css')}}" media="all" rel="stylesheet" type="text/css"/>
+    <link href="{{asset('css/app.css')}}" rel="stylesheet" type="text/css">
 
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+    <!-- Styles -->
+    <style>
 
-            .full-height {
-                height: 100vh;
-            }
+    </style>
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    <a href="{{ url('/login') }}">Login</a>
-                    <a href="{{ url('/register') }}">Register</a>
+</head>
+<body>
+<div class="row">
+    <div class="col s12 m4 l4"></div>
+    <div class="col s12 m4 l4">
+        <div id="card">
+            <div class="front">
+                <div class="no-margin card blue-grey white">
+                    <div class="card-content black-text no-padding">
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/1/1a/Code.jpg" alt="" id="welcome_image">
+                    </div>
+                    <div class="card-action" id="check">
+                        <input type="checkbox" id="accept"/>
+                        <label for="accept">Red</label>
+                    </div>
                 </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+            </div>
+            <div class="back">
+                <div class="no-margin card blue-grey white">
+                    <div class="card-content black-text">
+                        <span class="card-title">Card Title</span>
+                        <p>I am a very simple card. I am good at containing small bits of information.
+                            I am convenient because I require little markup to use effectively.</p>
+                    </div>
                 </div>
-                <ul class="collapsible" data-collapsible="accordion">
-                    <li>
-                        <div class="collapsible-header"><i class="mdi mdi-bell"></i>First</div>
-                        <div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
-                    </li>
-                    <li>
-                        <div class="collapsible-header"><i class="mdi mdi-account-box"></i>Second</div>
-                        <div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
-                    </li>
-                    <li>
-                        <div class="collapsible-header"><i class="mdi mdi-amazon"></i>Third</div>
-                        <div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
-                    </li>
-                </ul>
             </div>
         </div>
+    </div>
+    <div class="col s12 m4 l4"></div>
+</div>
 
-        <script type="text/javascript" src="{{asset('js/jquery.js')}}"></script>
-        <script type="text/javascript" src="{{asset('js/materialize.js')}}"></script>
-        <script>
-            $(document).ready(function(){
-                $('.collapsible').collapsible();
-            });
-        </script>
-    </body>
+
+<script type="text/javascript" src="{{asset('js/jquery.js')}}"></script>
+<script type="text/javascript" src="{{asset('js/materialize.js')}}"></script>
+<script type="text/javascript" src="{{asset('js/flip.js')}}"></script>
+<script>
+    $(document).ready(function () {
+        $("#card").flip({
+            trigger: 'manual',
+            speed: 1000
+        });
+    });
+
+    $("#accept").click(function () {
+        $("#card").flip('toggle');
+        $("#check").toggle("fast");
+    });
+</script>
+</body>
 </html>
